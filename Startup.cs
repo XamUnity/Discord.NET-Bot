f07f8a3e00bff39a8d88af157f88320602d632c4
XamUnity <97952123+XamUnity@users.noreply.github.com>
2022-01-19 22:21:33 +0800

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace DiscordBot
 {
     public class StartUp
     {
+        public IConfigurationRoot Configuration { get; }
+        public StartUp(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddYamlFile("_config.yml");
+            Configuration = builder.Build();
+        }
 
+        public static async Task RunAsync(string[] args)
+        {
+            
     }
 }
